709189f1b001f5caa3bc4607f43db68948d90047
adelson70 <bittencourtadelson1@gmail.com>
2025-09-14 18:33:29 -0300

    ajuste

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
 	const isDevelopment = process.env.NODE_ENV === 'development';
 
-	app.setGlobalPrefix('fluxo')
+	app.setGlobalPrefix('dialog')
 
 	app.use(helmet());
 	app.enableCors({
